cab1af0aab669e60f3c769c94dbf475ac3b5564b
AngdroRyu <cookmaster22@yahoo.com>
2025-09-16 21:07:53 -0500

    installed tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
+  <h1 class="">You did it!</h1>
+  <p class="font-bold">
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
 </template>
-
-<style scoped></style>
