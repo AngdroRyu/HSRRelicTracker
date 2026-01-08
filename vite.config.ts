b591548ffd85bb75197acb6cd1dd45955dcda850
AngdroRyu <cookmaster22@yahoo.com>
2026-01-07 23:44:36 -0600

    ready for test deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -7,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/HSRRelicTracker/',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
